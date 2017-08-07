7c14524dfbfe80470ca3931ceba1b1610af096c3
FormalHellhound <FormalHellhound@gmail.com>
2017-08-07 15:30:36 -0400

    Added a user plugin with slap and random name functions

---
+++ user/user.rb
@@ -0,0 +1,42 @@
+module BruhBot
+  module Plugins
+    # Choose plugin
+    module User
+      extend Discordrb::Commands::CommandContainer
+
+      # Load choose config file
+      user_conf = Yajl::Parser.parse(File.new("#{__dir__}/config.json", 'r'))
+
+      command(
+        :slap, min_args: 1,
+        description: 'Slap someone.',
+        usage: 'slap <user>'
+      ) do |event, user|
+
+        # Output a message from the choicemessage array in the config file,
+        # and insert a random choice from the ones provided
+        event.message.delete
+        user = user.gsub(/<@!?(\d+)>/) { |s| event.bot.user($1.to_i).on(event.server).id }
+        usermention = event.bot.member(event.server.id,user).mention
+        event.respond "#{event.user.display_name} slapped #{usermention}!"
+      end
+
+      command(
+        %s(nick.random), min_args: 0,
+        description: 'Get a random nickname',
+        usage: 'nick.random'
+      ) do |event|
+
+        name = user_conf['names'].sample
+
+        nick = "#{name}#{name} #{name}#{name}"
+        if nick.length > 32
+          nick = "#{name}#{name} #{name}#{name}"
+        else
+          event.bot.member(event.server.id, event.user.id).nick = nick
+        end
+        nil
+      end
+    end
+  end
+end
